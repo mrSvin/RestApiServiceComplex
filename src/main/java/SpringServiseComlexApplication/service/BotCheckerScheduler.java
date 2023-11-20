46c8ecc1c2f72ce02087a27cdd38b6de838b0f20
VasilievAV <vipal921@gmail.com>
2023-11-20 17:20:17 +0300

    commit

--- src/main/java/SpringServiseComlexApplication/service/BotCheckerScheduler.java
+++ src/main/java/SpringServiseComlexApplication/service/BotCheckerScheduler.java
@@ -4,6 +4,8 @@ import SpringServiseComlexApplication.repository.HistoryRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -16,14 +18,19 @@ public class BotCheckerScheduler {
         this.historyRepository = historyRepository;
     }
 
-    @Scheduled(cron = "0 39 16 * * ?") // запуск в 12:00 каждый день
+    @Scheduled(cron = "0 8 17 * * ?") // запуск в 12:00 каждый день
     public void botCheckerScheduler() {
         List<Map<String, Object>> data = historyRepository.serviceCheck();
+        long timeNow = System.currentTimeMillis();
+
         for (int i=0; i<data.size()-1; i++) {
-            System.out.println(data.get(i).get("complex_name"));
-            System.out.println(data.get(i).get("max_time"));
-            System.out.println(System.currentTimeMillis());
-            System.out.println(data.get(i).get("max_time").getClass());
+            Object complexName = data.get(i).get("complex_name");
+            Timestamp time = (Timestamp) data.get(i).get("max_time");
+
+            if (timeNow>time.getTime()) {
+
+            }
+
         }
 
     }
